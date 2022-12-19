1831467cdba40edcbac0958048dd88a7551e292f
mhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2022-12-19 20:47:49 +0600

    seed listing model

--- database/factories/ListingFactory.php
+++ database/factories/ListingFactory.php
@@ -17,7 +17,14 @@ class ListingFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'beds' => fake()->numberBetween(1, 7),
+            'baths' => fake()->numberBetween(1, 7),
+            'area' => fake()->numberBetween(30, 400),
+            'city' => fake()->city(),
+            'code' => fake()->postcode(),
+            'street' => fake()->streetName(),
+            'street_nr' => fake()->numberBetween(10, 200),
+            'price' => fake()->numberBetween(50_000, 2_000_000)
         ];
     }
 }
