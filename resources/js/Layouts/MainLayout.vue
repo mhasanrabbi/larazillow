d911014d10f1108491925d3cf72a8f4c20a46a94
mhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2022-12-20 13:40:13 +0600

    create listing with flash message

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,16 +1,35 @@
 <template>
   <Link href="/">Main Page</Link>&nbsp;
   <Link href="/hello">Show Page</Link>
-  <div>The page with time {{ timer }}</div>
-  <slot />
+  <!-- <div>The page with time {{ timer }}</div> -->
+  <div v-if="flashSuccess" class="success">
+    {{ flashSuccess }}
+  </div>
+  <div>{{ y }}</div>
+  <slot>Default</slot>
 </template>
 
 
 <script setup>
-import {Link} from '@inertiajs/inertia-vue3'
-import {ref} from 'vue'
+import { Link, usePage } from '@inertiajs/inertia-vue3';
+import { ref, computed } from 'vue';
 
-const timer = ref(0)
-setInterval(() => timer.value++, 1000)
+const x = ref(0);
+const y = computed(() => x.value * 2);
+// page.props.value.flash.success
+const page = usePage();
+const flashSuccess = computed(
+  () => page.props.value.flash.success,
+);
+// import { ref } from 'vue'
+
+// const timer = ref(0)
+// setInterval(() => timer.value++, 1000)
 </script>
 
+<style scoped>
+    .success {
+        background-color: green;
+        color: white;
+    }
+</style>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use App\Http\Controllers\IndexController;
+use App\Http\Controllers\ListingController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [IndexController::class, 'index']);
 Route::get('/hello', [IndexController::class, 'show']);
+
+Route::resource('listing', ListingController::class)->only(['index', 'show', 'create', 'store']);
