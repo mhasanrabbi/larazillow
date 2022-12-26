cdce1afd00769b9ab9fdbf52653f6aa4263221aa
mhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2022-12-26 19:34:05 +0600

    lodash debounce

--- resources/js/Pages/Listing/Index/Components/RealtorFilters.vue
+++ resources/js/Pages/Listing/Index/Components/RealtorFilters.vue
@@ -16,6 +16,7 @@
 <script setup>
 import {reactive, watch} from 'vue';
 import { Inertia } from '@inertiajs/inertia';
+import {debounce} from 'lodash';
 
 const filterForm = reactive({
   deleted: false,
@@ -24,10 +25,10 @@ const filterForm = reactive({
 // reactive / ref / computed
 
 watch(
-  filterForm, () => Inertia.get(
+  filterForm, debounce(() => Inertia.get(
     route('realtor.listing.index'),
     filterForm,
     {preserveState: true, preserveScroll: true},
-  ),
+  ), 1000),
 );
 </script>
