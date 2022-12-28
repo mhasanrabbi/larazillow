16b241e8fa8d64e0fecf764dbe44b49d5169977f
mhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2022-12-28 12:48:21 +0600

    restore models

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,16 @@ Route::delete('logout', [AuthController::class, 'destroy'])
 Route::resource('user-account', UserAccountController::class)
     ->only(['create', 'store']);
 
-Route::prefix('realtor')->name('realtor.')
-    ->middleware('auth')->group(function () {
-        Route::resource('listing', RealtorListingController::class)->only(['index', 'destroy', 'edit', 'update', 'create', 'store']);
+Route::prefix('realtor')
+    ->name('realtor.')
+    ->middleware('auth')
+    ->group(function () {
+        Route::name('listing.restore')
+            ->put(
+                'listing/{listing}/restore',
+                [RealtorListingController::class, 'restore']
+            )->withTrashed();
+        Route::resource('listing', RealtorListingController::class)
+            ->only(['index', 'destroy', 'edit', 'update', 'create', 'store'])
+            ->withTrashed();
     });
