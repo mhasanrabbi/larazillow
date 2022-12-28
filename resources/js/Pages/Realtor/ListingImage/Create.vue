e59f3138d1baeec6f56786f3f5a3b9c9dcd51a65
mhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2022-12-28 15:34:39 +0600

    displaying uploaded files

--- resources/js/Pages/Realtor/ListingImage/Create.vue
+++ resources/js/Pages/Realtor/ListingImage/Create.vue
@@ -2,23 +2,56 @@
   <Box>
     <template #header>Upload New Images</template>
     <form @submit.prevent="upload">
-      <input type="file" multiple @input="addFiles" />
-      <button type="submit" class="btn-outline">Upload</button>
-      <button type="reset" class="btn-outline" @click="reset">Reset</button>
+      <section class="flex items-center gap-2 my-4">
+        <input
+          class="border rounded-md file:px-4 file:py-2 border-gray-200 dark:border-gray-700 file:text-gray-700 file:dark:text-gray-400 file:border-0 file:bg-gray-100 file:dark:bg-gray-800 file:font-medium file:hover:bg-gray-200 file:dark:hover:bg-gray-700 file:hover:cursor-pointer file:mr-4"
+          type="file" multiple @input="addFiles"
+        />
+        <button
+          type="submit"
+          class="btn-outline disabled:opacity-25 disabled:cursor-not-allowed"
+          :disabled="!canUpload"
+        >
+          Upload
+        </button>
+        <button
+          type="reset" class="btn-outline"
+          @click="reset"
+        >
+          Reset
+        </button>
+      </section>
     </form>
+  </Box>
+  <Box v-if="listing.images.length" class="mt-4">
+    <template #header>Current Listing Images</template>
+    <section class="mt-4 grid grid-cols-3 gap-4">
+      <div v-for="image in listing.images" :key="image.id">
+        <img :src="image.src" class="rounded-md" />
+      </div>
+    </section>
   </Box>
 </template>
 
 <script setup>
 import Box from '@/Components/UI/Box.vue';
 import { useForm } from '@inertiajs/inertia-vue3';
+import { computed } from 'vue';
+import { Inertia } from '@inertiajs/inertia';
+import NProgress from 'nprogress';
 
 const props = defineProps({listing: Object});
+Inertia.on('progress', (event) => {
+  if (event.detail.progress.percentage) {
+    NProgress.set((event.detail.progress.percentage / 100) * 0.9);
+  }
+});
 
 const form = useForm({
   images: [],
 });
 
+const canUpload = computed(() => form.images.length);
 const upload = () => {
   form.post(
     route('realtor.listing.image.store', { listing: props.listing.id}),
