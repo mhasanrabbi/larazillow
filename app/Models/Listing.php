8c2b1de4f0f0f04ccc2b4b0159d3c0f846885b2b
mhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2022-12-28 13:10:33 +0600

    file data model

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Listing extends Model
@@ -27,6 +28,12 @@ class Listing extends Model
             'by_user_id'
         );
     }
+
+    public function images(): HasMany
+    {
+        return $this->hasMany(ListingImage::class);
+    }
+
     public function scopeMostRecent(Builder $query): Builder
     {
         return $this->orderByDesc('created_at');
