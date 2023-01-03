0178b21ba179d983b49a7bddde47e49e829fc839
mhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2023-01-03 18:00:59 +0600

    accepting offers

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ListingController;
 use App\Http\Controllers\ListingOfferController;
+use App\Http\Controllers\RealtorListingAcceptOfferController;
 use App\Http\Controllers\UserAccountController;
 use App\Http\Controllers\RealtorListingController;
 use App\Http\Controllers\RealtorListingImageController;
@@ -44,6 +45,12 @@ Route::prefix('realtor')
         Route::resource('listing', RealtorListingController::class)
             // ->only(['index', 'destroy', 'edit', 'update', 'create', 'store'])
             ->withTrashed();
+
+            Route::name('offer.accept')
+            ->put(
+                'offer/{offer}/accept',
+                RealtorListingAcceptOfferController::class
+            );
 
         Route::resource('listing.image', RealtorListingImageController::class)
             ->only(['create', 'store', 'destroy']);
