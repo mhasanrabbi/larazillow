dd4a75592dc7360e1844b46717db49c97102b7d9
mhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2023-01-04 14:56:05 +0600

    showing notification count

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,7 +21,7 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => '$2y$10$1uiGdRr6OuFbKiK.JAVmP.1uWAgirXlh0siPhFLbGK/0/0MYucQ52', // password
             'remember_token' => Str::random(10),
         ];
     }
