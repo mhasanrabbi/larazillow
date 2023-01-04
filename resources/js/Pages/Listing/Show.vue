5a5131f7196deff311e12ff09f6afea7bc7b20c6
mhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2023-01-04 15:09:41 +0600

    showing empty state

--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col-reverse md:grid md:grid-cols-12 gap-4">
-    <Box class="md:col-span-7 flex items-center">
-      <div v-if="listing.images.length" class="grid grid-cols-2 gap-1">
+    <Box v-if="listing.images.length" class="md:col-span-7 flex items-center">
+      <div class="grid grid-cols-2 gap-1">
         <img
           v-for="image in listing.images" :key="image.id"
           :src="image.src"
@@ -9,6 +9,7 @@
       </div>
       <div class="w-full text-center font-medium text-gray-500">No images</div>
     </Box>
+    <EmptyState v-else class="md:col-span-7 flex items-center">No images</EmptyState>
     <div class="md:col-span-5 flex flex-col gap-4">
       <Box>
         <template #header>
@@ -74,6 +75,7 @@ import {ref } from 'vue';
 import { usePage } from '@inertiajs/inertia-vue3';
 import { computed } from 'vue';
 import OfferMade from './Show/Components/OfferMade.vue';
+import EmptyState from '@/Components/UI/EmptyState.vue';
 import { useMonthlyPayment } from '@/Composables/useMonthlyPayment';
 
 const interestRate = ref(2.5);
